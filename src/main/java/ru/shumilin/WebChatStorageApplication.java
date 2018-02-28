0fa49f4b2062ca22b3961d49e87bec7a1a72eafc
Шумилин Александр Владимирович <shumilin2604@gmail.com>
2018-02-28 17:57:33 +0300

    commit

---
+++ src/main/java/ru/shumilin/WebChatStorageApplication.java
@@ -0,0 +1,11 @@
+package ru.shumilin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChatStorageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebChatStorageApplication.class,args);
+    }
+}
---
+++ src/main/java/ru/shumilin/controller/MainController.java
@@ -0,0 +1,16 @@
+package ru.shumilin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String mainPage(){
+        return "main";
+    }
+
+}
