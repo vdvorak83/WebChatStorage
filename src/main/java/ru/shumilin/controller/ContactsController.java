f733cf247bdab6506936d0105dd5a8838cc4d5b8
Unknown <shumilin2604@gmail.com>
2018-03-11 22:35:51 +0300

    import nav bar

--- src/main/java/ru/shumilin/controller/ContactsController.java
+++ src/main/java/ru/shumilin/controller/ContactsController.java
@@ -1,7 +1,12 @@
 package ru.shumilin.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ContactsController {
+    @RequestMapping("/about")
+    public String aboutPage(){
+        return "about";
+    }
 }
